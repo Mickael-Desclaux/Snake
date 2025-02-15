4a91ae00a3f0c273cfbdc256e82d4f7acdc696cb
Mickael <mickaeldesclaux@gmail.com>
2025-02-15 16:01:23 +0100

    Add Apple

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Apple : MonoBehaviour
+{
+    private int _positionXMax = 10;
+    private int _positionYMax = 10;
+
+    private void Start()
+    {
+        int randomX = Random.Range(-_positionXMax, _positionXMax);
+        int randomY = Random.Range(-_positionYMax, _positionYMax);
+
+        transform.position = new Vector3(randomX, randomY, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UpdatePosition();
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 lastPosition = currentPosition;
+
+        while (lastPosition == currentPosition)
+        {
+            int randomX = Random.Range(-_positionXMax, _positionXMax);
+            int randomY = Random.Range(-_positionYMax, _positionYMax);
+
+            currentPosition = new Vector3(randomX, randomY, 0);
+        }
+
+        transform.position = currentPosition;
+    }
+}
