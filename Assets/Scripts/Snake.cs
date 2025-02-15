ab2619b3c3e66afc6fa4231b295954195de9f24c
Mickael <mickaeldesclaux@gmail.com>
2025-02-15 15:21:07 +0100

    Handle Player Input

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -15,8 +15,29 @@ public class Snake : MonoBehaviour
         transform.position += _direction * Time.deltaTime;
     }
 
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _direction != Vector3.down)
+        {
+            _direction = Vector3.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && _direction != Vector3.up)
+        {
+            _direction = Vector3.down;
+        } 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && _direction != Vector3.right)
+        {
+            _direction = Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && _direction != Vector3.left)
+        {
+            _direction = Vector3.right;
+        }
+    }
+
     private void LateUpdate()
     {
+        HandleInput();
         Move();
     }
 }
