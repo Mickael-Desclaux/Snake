b6ed6ac597eb87c70ad813ae6087790f78f0ed48
Mickael <mickaeldesclaux@gmail.com>
2025-02-15 19:41:06 +0100

    Add Eat method and Tail system

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,17 +1,23 @@
-using System;
-using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+    [SerializeField] private Transform _tailPrefab;
     [SerializeField] private float _interval = 0.1f;
     private Vector3 _direction;
+    private Vector3 _nextDirection;
+    private List<Transform> _tailParts;
+    private Vector3 _lastTailPosition;
 
     private void Start()
     {
-        _direction = Vector3.right;
+        _tailParts.Add(transform);
+        _nextDirection = Vector3.right;
         InvokeRepeating(nameof(Move), 0, _interval);
     }
+    
     private void LateUpdate()
     {
         HandleInput();
@@ -19,32 +25,55 @@ public class Snake : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        Apple apple = FindFirstObjectByType<Apple>();
-        apple.UpdatePosition();
+        Eat();
     }
 
     private void Move()
     {
+        if (_direction == -_nextDirection)
+        {
+            return;
+        }
+
+        _direction = _nextDirection;
+        
+        _lastTailPosition = _tailParts.Last().position;
+        
+        for (int i = _tailParts.Count - 1; i > 0; i--)
+        {
+            Vector3 newPosition = _tailParts[i - 1].position;
+            _tailParts[i].position = newPosition;
+        }
+
         transform.position += _direction;
+    }
+
+    private void Eat()
+    {
+        Apple apple = FindFirstObjectByType<Apple>();
+        apple.UpdatePosition();
+        
+        Transform newTail = Instantiate(_tailPrefab, _lastTailPosition, Quaternion.identity);
+        _tailParts.Add(newTail);
     }
 
     private void HandleInput()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && _direction != Vector3.down)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _nextDirection != Vector3.down)
         {
-            _direction = Vector3.up;
+            _nextDirection = Vector3.up;
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && _direction != Vector3.up)
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && _nextDirection != Vector3.up)
         {
-            _direction = Vector3.down;
+            _nextDirection = Vector3.down;
         } 
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && _direction != Vector3.right)
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) && _nextDirection != Vector3.right)
         {
-            _direction = Vector3.left;
+            _nextDirection = Vector3.left;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && _direction != Vector3.left)
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && _nextDirection != Vector3.left)
         {
-            _direction = Vector3.right;
+            _nextDirection = Vector3.right;
         }
     }
 }
