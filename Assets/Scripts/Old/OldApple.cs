8cd4bddfbd6de6790bcc56e03d5b73d604a4860f
Mickael <mickaeldesclaux@gmail.com>
2025-02-16 14:21:45 +0100

    Moved Snake and Apple to old folder

---
+++ Assets/Scripts/Old/OldApple.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Old
+{
+    public class OldApple : MonoBehaviour
+    {
+        private int _positionXMax = 12;
+        private int _positionYMax = 6;
+
+        private void Start()
+        {
+            int randomX = Random.Range(-_positionXMax, _positionXMax);
+            int randomY = Random.Range(-_positionYMax, _positionYMax);
+
+            transform.position = new Vector3(randomX, randomY, 0);
+        }
+
+        public void UpdatePosition()
+        {
+            Vector3 currentPosition = transform.position;
+            Vector3 lastPosition = currentPosition;
+
+            while (lastPosition == currentPosition)
+            {
+                int randomX = Random.Range(-_positionXMax, _positionXMax);
+                int randomY = Random.Range(-_positionYMax, _positionYMax);
+
+                currentPosition = new Vector3(randomX, randomY, 0);
+            }
+
+            transform.position = currentPosition;
+        }
+    }
+}
